6b3b6d598d9b507e9ef9304a9423f33fb0ee1b97
AngelMoraDev <angelmomaa@gmail.com>
2021-02-01 21:20:14 -0600

    fix integration test

--- test/integration/user_friendships.rb
+++ test/integration/user_friendships.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class UserFlowsTest < ActionDispatch::IntegrationTest
+class UserFriendshipsTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
