e2fdc8357a66b5f6ce27cfdab8c4fe3b6131702c
AngelMoraDev <angelmomaa@gmail.com>
2021-03-05 19:03:40 -0600

    fix errors

--- spec/user_test.rb
+++ spec/user_test.rb
@@ -6,11 +6,10 @@ RSpec.describe User, type: :model do
   let(:email) { 'user@example.com' }
   let(:password) { 'admin123' }
   let!(:user) { User.create(name: name, email: email, password: password) }
-    
+
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
   it { should validate_length_of({ maximum: 20 }) }
   it { should have_many(:friendships) }
   it { should have_many(:inverse_friendships) }
-
 end
