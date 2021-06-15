56be6e6a193346249976791edab5c0fc18d99ec6
AngelMoraDev <angelmomaa@gmail.com>
2021-06-15 18:56:07 -0500

    add friendships methods

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,21 +4,36 @@ class FriendshipsController < ApplicationController
     @friendship.confirmed = false
     if @friendship.save
       flash[:notice] = 'Friend request was successfully sent.'
-      redirect_to root_url
+      redirect_to request.referrer
     else
-      redirect_to root_url, alert: @friendship.errors.full_messages.join('. ').to_s
+      redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
     end
   end
 
   def update
+    @friendship = Friendship.find(params[:id])
+    @friendship.confirmed = true
 
+    if @friendship.save
+      @friendship.confirm_friend
+      redirect_to request.referrer, notice: 'Friend request was successfully confirmed.'
+    else
+      redirect_to request.referrer, alert: @friendship.error.full_messages.join('. ').to_s
+    end
   end
 
   def index
-    
+    @friendships = current_user.friendships
+    @inverse_friendships = current_user.inverse_friendships
   end
 
   def destroy
-    
+    @friendship = Friendship.find_by(friend_id: current_user.id, user_id: params[:user_id])
+
+    if @friendship.destroy
+      redirect_to request.referrer, notice: 'Friend request declined'
+    else
+      redirect_to request.referrer, alert: @friendship.errors.full_messages.join('. ').to_s
+    end
   end
 end
