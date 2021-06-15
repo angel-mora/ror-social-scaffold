a830f70bc86edaf2f36e6bf2089170c128a7ea94
AngelMoraDev <angelmomaa@gmail.com>
2021-06-15 17:19:44 -0500

    fix shoulda-matchers to validate user model

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 require 'spec_helper'
+require 'shoulda-matchers'
 
 RSpec.describe User, type: :model do
-  let(:name) { 'Juan' }
-  let(:email) { 'user@example.com' }
-  let(:password) { 'admin123' }
-  let!(:user) { User.create(name: name, email: email, password: password) }
-
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
-  it { should validate_length_of({ maximum: 20 }) }
-  it { should have_many(:friendships) }
-  it { should have_many(:inverse_friendships) }
+  let(:user) { User.create(name: 'Juan', email: 'user@example.com', password: 'admin123') }
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_length_of({ maximum: 20 }) }
+    it { should have_many(:friendships) }
+    it { should have_many(:inverse_friendships) }
+  end
 end
