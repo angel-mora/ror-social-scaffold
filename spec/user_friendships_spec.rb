171e23d050aa570781af28004127451d518c8b24
AngelMoraDev <angelmomaa@gmail.com>
2021-06-15 17:10:00 -0500

    pass one test to get back on track

--- spec/user_friendships_spec.rb
+++ spec/user_friendships_spec.rb
@@ -3,36 +3,27 @@ require 'spec_helper'
 
 RSpec.describe User, type: :feature do
   # User 1
-  let(:name1) { 'Juan' }
-  let(:email1) { 'user@example.com' }
-  let(:password1) { 'admin123' }
-  let!(:user1) { User.create(name: name1, email: email1, password: password1) }
+  let(:user1) { User.create(name: 'Juan', email: 'user@example.com', password: 'admin123') }
 
   # User 2
-  let(:name2) { 'Pedro' }
-  let(:email2) { 'admin@microsoft.com' }
-  let(:password2) { 'admin123' }
-  let!(:user2) { User.create(name: name2, email: email2, password: password2) }
-
-  context 'when user is not logged in' do
+  let(:user2) { User.create(name: 'Pedro', email: 'admin@microsoft.com', password: 'admin123') }
+  describe 'friendship' do
     before do
-      visit new_userregistration_path
-    end
-    scenario 'user not logged-in' do
-      fill_in 'session_email', with: :email
-      fill_in 'session_password', with: :password
-      click_button 'Sign Up'
+      visit new_user_session_path
+      fill_in 'user[email]', with: user1.email
+      fill_in 'user[password]', with: user1.password
+      click_button 'Log in'
     end
     it 'shows the right content for user show' do
-      visit usershow_path
+      visit users_path
       expect(page).to have_content('Add Friend')
     end
     it 'updates Friendship button pending status' do
-      visit usershow_path
+      visit users_path
       expect(page).to have_content('Pending')
     end
     it 'updates Friendship button delete status' do
-      visit usershow_path
+      visit users_path
       expect(page).to have_content('Delete Friend')
     end
   end
